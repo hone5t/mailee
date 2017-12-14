36581b221ace45086d5c83320ad8b31629f5bf83
hone5t <hone5t@hone5t.com>
2017-12-14 13:40:16 -0600

    save info using localstorage so the user dont have to rewrite the notification each time

--- public/mailee_ext/src/lib/browser_action.js
+++ public/mailee_ext/src/lib/browser_action.js
@@ -1,9 +1,24 @@
 var editor;
 var smtpSettings = {};
 
+function saveToStorage(key, val) {
+    // chrome.storage.sync.set({key: val}, () => {});
+    if (key == 'msg' && !val){
+        // dont allow the msg to be empty 
+        return;
+    }
+    localStorage.setItem(key,val);
+}
+
+function getFromStorage(key) {
+    return localStorage.getItem(key);
+}
+
 const formToJSON = elements => [].reduce.call(elements, (data, element) => {
-    if( element.name)
+    if( element.name) {
         data[element.name] = element.value;
+        saveToStorage(element.name, element.value);
+    }
     return data;
 }, {});
 
@@ -13,6 +28,7 @@ function popup(data,status,xhr) {
     chrome.tabs.sendMessage(activeTab.id , {"data": data});
    });
 }
+
 
 function loadChange(event,options)
 {
@@ -32,9 +48,11 @@ function loadChange(event,options)
 function setOptions () {
     chrome.storage.sync.get('maileeOptions',(obj)=>{
         smtpSettings = obj.maileeOptions;
-        $('#username').val(smtpSettings.username);
-        $('#password').val(smtpSettings.password);
-        $('#email').val(smtpSettings.email);
+        if (smtpSettings) {
+            $('#username').val(smtpSettings.username);
+            $('#password').val(smtpSettings.password);
+            $('#email').val(smtpSettings.email);
+        }
     });
 }
 
@@ -48,7 +66,18 @@ $(document).ready(function(){
         crossDomain: true,
         success: (data,textStatus,jqXHR)=>{$('#result').html(data);}
     }
+    variables = ['link_footer_name','link_footer','footer_content','action_link', 'action_text', 'intro', 'title', 'preheader'];
+    variables.forEach((element)=>{
+        if (getFromStorage(element)) {
+            ele = document.getElementsByName(element)[0];
+            ele.value = getFromStorage(element);
+        }
+    });
     editor = ace.edit("editor");
+    if (localStorage.getItem('msg')) {
+        editor.setValue(getFromStorage('msg'), 1);
+    }
+
     $('#preview').click(function(e){
         loadChange(e,options);  
     });
